ad39b5d823d80804616b74f76ebc07930a730eb7
Oleksandr Sekker <oleksandr.sekker@gmail.com>
2024-10-16 11:51:52 +0300

    Add default handlers to ContactForm story args
    New default functions for `handleSubState` and `setCloseModal` were added to the ContactForm story args. These changes ensure that the component can function correctly in Storybook without requiring additional context or external functions.

--- src/components/ContactForm/ContactForm.stories.ts
+++ src/components/ContactForm/ContactForm.stories.ts
@@ -16,5 +16,8 @@ type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const ContactFormComponent: Story = {
-  args: {},
+  args: {
+    handleSubState: () => {},
+    setCloseModal: () => {},
+  },
 };
